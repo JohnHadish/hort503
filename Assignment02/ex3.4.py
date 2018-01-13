3a654995c69152c9575829472f89bb0ae060479d
JohnHadish <john.hadish@wsu.edu>
2018-01-12 17:25:55 -0800

    adding Assignment 1-5

---
+++ Assignment02/ex3.4.py
@@ -0,0 +1,28 @@
+print("I will now count my chickens:")
+
+#This counts Chickens, both male and female chickens, in fact
+print("Hens", float(25 + 30 / 6))
+print("Roosters", float(100 - 25 * 3 % 4))
+
+print("Now I will count the eggs:")
+
+#This counts some delicious eggs
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#This determines if 3 + 2 is less than 5 - 7
+print(float(3 + 2 < 5 - 7))
+
+#This breaks apart above equation
+print("Whaty is 3 + 2?", float(3 + 2))
+print("What is 5 - 7", float(5 - 7))
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+#These explore the relationship between 5 and -2
+print("Is it greater?", float(5 > -2))
+print("Is it greater or equal?", float(5 >= -2))
+print("Is it less or equal?", float(5 <= -2))
