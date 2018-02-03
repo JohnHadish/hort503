32a665b7f859f71c0dde871df6b523244539a4a1
JohnHadish <john.hadish@wsu.edu>
2018-02-02 17:53:04 -0800

    ex43

---
+++ Assignment05/ex43.py
@@ -0,0 +1,56 @@
+class Scene():
+
+    def enter(self):
+        pass
+
+class Engine():
+
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        while True:
+            user_input = input("> ")
+            user_input_list = user_input.split(' ')
+            words
+            if user_input
+#These are all "scenes"
+class Death(Scene):
+
+    def enter(self):
+            pass
+
+class Central_Corridor(Scene):
+
+    def enter(self):
+        pass
+
+class Laser_Weapon_Armory(Scene):
+
+    def enter(self):
+        pass
+
+class The_Bridge(Scene):
+
+    def enter(self):
+        pass
+class Escape_Pod(Scene):
+
+    def enter(self):
+        pass
+
+#This is where I will record which directions a player can go
+class Map():
+
+    def __init__(self, start_scene):
+        pass
+
+    def nextScene():
+        pass
+
+    def openingScene():
+        pass
+
+a_map = Map('Central_Corridor')
+a_game = Engine(a_map)
+a_game.play()
