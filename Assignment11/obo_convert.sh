a55d4b2737597d0125231cdd1bbbf86476f4aa7a
JohnHadish <john.hadish@wsu.edu>
2018-03-26 20:14:41 -0700

    assignment 11

--- Assignment11/obo_convert.sh
+++ Assignment11/obo_convert.sh
@@ -1,1 +1,1 @@
-grep -E --line-buffered '^id:|^name:|^namespace:|^def:' goslim_plant.obo | sed 's/^.*: //' | sed 'N;N;N;s/\n/\t/g' | grep '^GO:'
+grep -E --line-buffered '^id:|^name:|^namespace:|^def:' goslim_plant.obo | sed 's/^.*: //' | sed 'N;N;N;s/\n/\t/g' | grep '^GO:' > outfile.txt
